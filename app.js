e5f25642dfab7b8acbe01499df36d4393c8c903f
kshitiz-orizion <kshitiz.orizion@gmail.com>
2017-07-19 00:09:45 +0530

    done all todo functions

--- app.js
+++ app.js
@@ -16,75 +16,53 @@ app.set('port',process.env.PORT||3000);
 app.set('views',__dirname+'views');
 app.set('view engine','jade');
 app.use(express.static(path.join(__dirname,'public')));
+ 
+mongoose.connect('mongodb://localhost/Todo');
+var assert = require('assert');
+
+var Todo = mongoose.model('Todo', mongoose.Schema({
+  number:{type:Number,unique:true},
+  task:String
+}));
 app.use(bodyParser.json()); 
 app.use(bodyParser.urlencoded({ 
   extended: true
-})); 
-var MongoClient = require('mongodb').MongoClient;
-var assert = require('assert');
-
-var url = 'mongodb://localhost:27017/Todo';
-MongoClient.connect(url, function(err, db) {
-  assert.equal(null, err);
-  console.log("Connected correctly to server");
-
-});
-var TodoSchema=new mongoose.Schema({
-  id:{type:Number, unique:true},
-task:String
-});
-
-var todo=mongoose.model('todo',TodoSchema);
+}));
 app.get('/gettodo', function (req, res) {
     console.log('I received a GET request');
-    MongoClient.connect(url,function(err,db){
-    db.collection('Todo').find({}).toArray(function(err,docs){
-      res.json(docs);
-    
-    db.close();
-    });
-});
+    Todo.find(function(err, todo){
+      console.log(todo);
+    if(err)
+      res.send(err);
+    res.json(todo);
+  });
 });
 app.post('/posttodo', function(req, res) {
-  var todo={
-      id:req.body.id,
-      task:req.body.task
+ Todo.create( req.body, function(err, todo){
+    if(err)
+      res.send(err);
+    res.json(todo);
+  });
+});
+app.delete('/deletetodo/:id', function(req, res){
+  Todo.findOneAndRemove({_id:req.params.id},function(err,todo){
+    if(err)
+      res.send(err);
+    res.json(todo);
+  });
+});
+app.put('/updatetodo/:id', function(req, res){
+  var query = {
+    number:req.body.number,
+    task:req.body.task
   };
-  MongoClient.connect(url,function(err,db){
-    assert.equal(null,err);
-    db.collection('Todo').update(todo,todo,{upsert:true},function(err,result){
-      assert.equal(null,err);
-      console.log("item inserted");
-      db.close();
-    });
-  });
-});
-app.delete('/deletetodo/:id',function(req,res){
-  console.log('I received a Delete request');
-   MongoClient.connect(url,function(err,db){
-    
-    db.collection('Todo').findOneAndDelete({id:req.params.id},function(err,todo){
-      if(err) throw err;
-      console.log("item deleted");
-      res.json(todo);
-      db.close();
-    });
-  });
-
-});
-app.put('/updatetodo/:id',function(req,res){
-    console.log("I recieved a update request");
-    MongoClient.connect(url,function(err,db){
-    
-    db.collection('Todo').findOneAndUpdate({id:req.params.id},{
-      $set:{task:req.body.task}
-    },{
-      new:true
-    },function(err,updatedtodo){
-      if(err) throw err;
-      res.json(updatedtodo);
-    }
-    );
+  console.log(req.body.number);
+  console.log(req.params.id);
+  console.log(req.body.task);
+  Todo.findOneAndUpdate({_id:req.params.id}, query, function(err, todo){
+    if(err)
+      res.send(err);
+    res.json(todo);
   });
 });
 app.listen(3000,function(err,data){
